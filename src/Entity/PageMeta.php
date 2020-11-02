cb96f6bb6a3b02b4996c46260c5160d9fa424d60
piotrfejklowicz <piotrf5bin@o2.pl>
2020-11-02 22:33:22 +0100

    Add new Controlers and Entities

---
+++ src/Entity/PageMeta.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PageMetaRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=PageMetaRepository::class)
+ */
+class PageMeta
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $slug;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $parent;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $fieldType;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $postId;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): self
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+
+    public function getParent(): ?int
+    {
+        return $this->parent;
+    }
+
+    public function setParent(?int $parent): self
+    {
+        $this->parent = $parent;
+
+        return $this;
+    }
+
+    public function getFieldType(): ?string
+    {
+        return $this->fieldType;
+    }
+
+    public function setFieldType(string $fieldType): self
+    {
+        $this->fieldType = $fieldType;
+
+        return $this;
+    }
+
+    public function getPostId(): ?int
+    {
+        return $this->postId;
+    }
+
+    public function setPostId(?int $postId): self
+    {
+        $this->postId = $postId;
+
+        return $this;
+    }
+}
